086fa2820ffc1aea61e67f20d079699a9670a1d6
Connor Dowson <connormwdowson@gmail.com>
2017-04-16 15:34:13 +0100

    Added files

--- js/slideshow.js
+++ js/slideshow.js
@@ -2,56 +2,103 @@ $(function(){
 
 
 var homeImages = ["img/uni.jpg", "img/house.jpg"];
-var counter = 0;
-var length = homeImages.length;
+var homeCounter = 0;
+var homeLength = homeImages.length;
+
+var accomodationCounter = 0;
+var formalPlaceImages = ["img/formal_place/1.jpg", "img/formal_place/2.jpg", "img/formal_place/3.jpg", "img/formal_place/4.jpg", "img/formal_place/5.jpg"];
+var formalPlaceLength = formalPlaceImages.length;
+
+
+var nextImage = function(){
+
+        //find out which page it is
+
+        if($("#next").hasClass("formal-place")){
+
+            if(accomodationCounter > (formalPlaceLength - 1)){
+
+                accomodationCounter = 0;
+                $(".column img").prop("src", formalPlaceImages[accomodationCounter]);
+
+            } else {
+
+                $(".column img").prop("src", formalPlaceImages[accomodationCounter]);
+
+            }
+
+        }
+
+        if($("#next").hasClass("henley-court")){
+
+            if(accomodationCounter > (henleyCourtLength - 1)){
+
+                accomodationCounter = 0;
+                $(".column img").prop("src", henleyCourtImages[accomodationCounter]);
+
+            } else {
+
+                $(".column img").prop("src", henleyCourtImages[accomodationCounter]);
+
+            }
+
+        }
+
+
+
+};
+
+
+
+$("#next").click(function(){
+
+    accomodationCounter++;
+    nextImage();
+
+});
+
+$("#previous").click(function(){
+
+    accomodationCounter--;
+
+    if(accomodationCounter < 0){
+
+        accomodationCounter = (formalPlaceLength - 1);
+        $(".column img").prop("src", formalPlaceImages[accomodationCounter]);
+
+    }
+    else{
+
+        $(".column img").prop("src", formalPlaceImages[accomodationCounter]);
+
+    }
+});
+
+//Homepage timer slideshow
 
 var updateImage = function() {
 
-    $("#home-slideshow img").prop("src", homeImages[counter]);
+    $("#home-slideshow img").prop("src", homeImages[homeCounter]);
 
 }
 
-// $("#next").click(function(){
-//
-//     $("#home-slideshow img").prop("src", homeImages[counter+1]);
-//     counter++;
-//     if(counter > (length - 1)){
-//         counter = 0;
-//         $("#home-slideshow img").prop("src", homeImages[counter]);
-//     }
-// });
-//
-// $("#previous").click(function(){
-//
-//     $("#home-slideshow img").prop("src", homeImages[counter-1]);
-//     counter--;
-//     if(counter < 0){
-//         counter = (length - 1);
-//         $("#home-slideshow img").prop("src", homeImages[counter]);
-//
-//     }
-// });
-
 setInterval(function(){
 
-    if(counter == 0){
+    if(homeCounter == 0){
 
-        counter++;
-        console.log(counter);
+        homeCounter++;
         updateImage();
 
-    } else if (counter = (length)){
+    } else if (homeCounter = (homeLength - 1)){
 
-        counter = 0;
-        console.log(counter);
+        homeCounter = 0;
         updateImage();
 
     }
 
-    else if( 0 < counter < length ){
+    else if( 0 < homeCounter < homeLength ){
 
-        counter++;
-        console.log(counter);
+        homeCounter++;
         updateImage();
 
     }
